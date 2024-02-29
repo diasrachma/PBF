a9f175d59cbf72da0564512c97c29507f4a72d20
diasrachma <diasrachma.dr@gmail.com>
2024-02-29 11:47:51 +0700

    W03: Jawaban soal 2

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -1,17 +1,29 @@
-import Image from "next/image";
-import Profile from "../components/profile";
+// import { Gallery } from "@/components/gallery";
+// import Image from "next/image";
+// import Profile from "../components/profile";
+
+// export default function Home() {
+//   return (
+// <div>
+//   <h2 className="text-center font-bold">Ilmuwan yang luar biasa</h2>
+//   <div className="grid grid-cols-3 gap-8 justify-center">
+//       <Profile></Profile>
+//       <Profile></Profile>
+//       <Profile></Profile>
+//     </div>
+// </div>
+//   );
+// }
+
+import { Gallery } from "@/components/gallery";
 
 export default function Home() {
   return (
-<div>
-  <h2 className="text-center font-bold">Ilmuwan yang luar biasa</h2>
-  <div className="grid grid-cols-3 gap-8 justify-center">
-      <Profile></Profile>
-      <Profile></Profile>
-      <Profile></Profile>
-    </div>
-</div>
-
-
+    <div>
+      <h1 className="text-center">Ilmuwan yang luar biasa</h1>
+      <div className="grid grid-cols-3 gap-8 justify-center">
+     <Gallery/>
+  </div></div>
+    
   );
 }
