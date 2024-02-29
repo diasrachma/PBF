2660a813deaa00e17474f2d852e26886739bb226
diasrachma <diasrachma.dr@gmail.com>
2024-02-29 12:41:22 +0700

    W03: Jawaban soal 3

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -15,15 +15,48 @@
 //   );
 // }
 
+// // import { Gallery } from "@/components/gallery";
+
+// // export default function Home() {
+// //  return (
+// //    <div>
+// //      <h1 className="text-center">Ilmuwan yang luar biasa</h1>
+//       <div className="grid grid-cols-3 gap-8 justify-center">
+//      <Gallery/>
+//   </div></div>
+    
+//   );
+// }
+
+// import { Gallery } from "@/components/gallery";
+// import TodoList from "@/components/todolist";
+
+// export default function Home() {
+//   return (
+//     <section>
+//       <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
+//       <hr />
+//       <Gallery />
+//       <hr />
+//       <TodoList />
+//     </section>
+//   );
+// }
+
 import { Gallery } from "@/components/gallery";
+import TodoList from "@/components/todolist";
+import Bio from "@/components/bio";
 
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-center">Ilmuwan yang luar biasa</h1>
+ return (
+   <div>
+     <h1 className="text-center">Ilmuwan yang luar biasa</h1>
       <div className="grid grid-cols-3 gap-8 justify-center">
      <Gallery/>
-  </div></div>
-    
-  );
+  </div>
+  <Bio/>
+  <TodoList/>
+  </div>
+
+ );
 }
---
+++ 03-belajar-komponen/my-app/src/components/bio.jsx
@@ -0,0 +1,13 @@
+export default function Bio() {
+    return (
+        <>
+        <div className="intro">            
+        </div>
+        <p className="summary">
+            Anda dapat membaca uneg-unegku disini.
+            <br/><br/>
+            <b>Juga ada <i>foto</i> ilmuwan!</b>
+        </p>
+        </>
+    );
+}
