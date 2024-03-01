ad68e902f9d7c4e1c862a4f0d5f1b47dbfa69771
diasrachma <diasrachma.dr@gmail.com>
2024-03-01 14:12:35 +0700

    W03: Jawaban soal 7

--- 03-belajar-komponen/my-app/src/components/bio.jsx
+++ 03-belajar-komponen/my-app/src/components/bio.jsx
@@ -1,11 +1,20 @@
-export default function Bio() {
+// export default function Bio() {
+//     return (
+//       <div className="intro">
+//         <h1> Selamat datang di website saya</h1>
+//         <p className="summary">
+//           Anda dapat membaca uneg-uneg di sini. <br></br>
+//           <b>Juga ada <i>foto</i></b> Ilmuwan
+//         </p>
+//       </div>
+//     );
+//   }
+
+  export function getImageUrl(person, size = 's') {
     return (
-      <div className="intro">
-        <h1> Selamat datang di website saya</h1>
-        <p className="summary">
-          Anda dapat membaca uneg-uneg di sini. <br></br>
-          <b>Juga ada <i>foto</i></b> Ilmuwan
-        </p>
-      </div>
+      'https://i.imgur.com/' +
+      person.imageId +
+      size +
+      '.jpg'
     );
   }
--- 03-belajar-komponen/my-app/src/utils/utils.tsx
+++ 03-belajar-komponen/my-app/src/utils/utils.tsx
@@ -6,3 +6,11 @@ export function getImageUrl(imageId : String, size = 's') {
     '.jpg'
   );
 }
+export function getImageUrlV2(imageId : String, size = 'b') {
+    return (
+      'https://i.imgur.com/' +
+      imageId +
+      size +
+      '.jpg'
+    );
+  }
