f39196642867da704075cd74a20731f647341691
diasrachma <diasrachma.dr@gmail.com>
2024-03-06 08:56:50 +0700

    Praktikum 1

---
+++ 04-event-dan-state/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Tombol_1 from "@/components/button";
+
+export default function Home() {
+  return (
+    <>
+    <div className="container mx-auto">
+      <h2>Kuis Kota</h2>
+      <Tombol_1 />
+    </div>
+    </>
+  );
+}
