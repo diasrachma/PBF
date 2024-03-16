3fc9e1eb023d6e4047d73d1fdf59aed487725d49
diasrachma <diasrachma.dr@gmail.com>
2024-03-16 21:06:23 +0700

    update read me

--- routing-demo-main/src/app/blogs/page.tsx
+++ routing-demo-main/src/app/blogs/page.tsx
@@ -6,8 +6,8 @@ export default function Blog() {
         <div>
             <h1>Blog List</h1>
             <ul>
-                <li><Link href='/blogs/1first'>Blog 1</Link></li>
-                <li><Link href='blogs/2second'>Blog 2</Link></li>
+                <li><Link href='/blogs/first'>Blog 1</Link></li>
+                <li><Link href='blogs/second'>Blog 2</Link></li>
             </ul>
         </div>
     );
