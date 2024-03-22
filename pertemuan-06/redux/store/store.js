99f3edf12fee4b6a336d3deb6ae598a30d85ec7b
diasrachma <diasrachma.dr@gmail.com>
2024-03-22 18:51:29 +0700

    Praktikum 2

---
+++ pertemuan-06/redux/store/store.js
@@ -0,0 +1,30 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import authReducer from '../auth/authSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import counterReducer from '../counter/naikTurunSlice';
+
+const persistConfig = {
+    key: process.env.NEXT_PUBLIC_FINGERPRINT_NAME || 'root',  // Menggunakan nilai default 'root' jika NEXT_PUBLIC_FINGERPRINT_NAME tidak diatur
+    storage,
+    whitelist: ['auth'],
+};
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    counter: counterReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+const persistor = persistStore(store);
+export { store, persistor };
