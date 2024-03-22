1a2c892ae23cb0f05f138343450a96789d2d4a54
diasrachma <diasrachma.dr@gmail.com>
2024-03-22 19:05:20 +0700

    Praktikum 2

--- pertemuan-06/pages/login.tsx
+++ pertemuan-06/pages/login.tsx
@@ -1,41 +1,39 @@
-import { useDispatch,useSelector } from "react-redux";
-import {setLogin} from '../redux/auth/authSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { setLogin } from '../redux/auth/authSlice';
 import "bootstrap/dist/css/bootstrap.min.css";
 import parse from 'html-react-parser';
 
-
-export default function LoginCheck(){
-    const {isLogin} = useSelector((state)=>state.auth);
+export default function LoginCheck() {
+    const { isLogin } = useSelector((state) => state.auth);
     const dispatch = useDispatch();
 
-    function handleAuth(type = 'logout'){
-        if(type === 'logout' || type === 'login'){
-            dispatch(setLogin({isLogin:type === 'login'?true:false}));
+    function handleAuth(type = 'logout') {
+        if (type === 'logout' || type === 'login') {
+            dispatch(setLogin({ isLogin: type === 'login' ? true : false }));
         }
     }
 
-    return(
+    return (
         <div className="container">
             <div className="row">
                 <div className="col-12">
                     <div className="card mt-3">
-                        <div className="card-header">Status Login</div>
+                        <div className="card-header">status Login</div>
                         <div className="card-body">
-                            {isLogin?
-                            [
-                                // parse('<div className="alert alert-success">Yay, berhasil login!!!</div>'),
-                                <button className="btn btn-md btn-danger" onClick={()=> handleAuth('logout')}>Log out</button>
+                            {isLogin ? [
+                                parse('<div class="alert alert-success">Yay, berhasil login!!!</div>'),
+                                <button className="btn btn-md btn-danger" onClick={() => handleAuth('logout')}>Log out</button>
                             ]
-                        :
-                        [
-                            // parse('<div className="alert alert-dark">Anda telah logout!</div>'),
-                                    <button className="btn btn-md btn-primary" onClick={() => handleAuth('login')}>Log in</button>
-                        ]
-                        }
+                            :
+                            [
+                                parse('<div class="alert alert-dark">Anda telah logout!</div>'),
+                                <button className="btn btn-md btn-primary" onClick={() => handleAuth('login')}>Log in</button>
+                            ]
+                            }
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
