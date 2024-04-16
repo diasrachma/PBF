2b91d9cb2aceebd17466423c537a590599ca6cca
diasrachma <diasrachma.dr@gmail.com>
2024-04-16 20:59:22 +0700

    Laravel NextJS

--- 07-belajar-context-nextjs/src/components/atoms/myapp.tsx
+++ 07-belajar-context-nextjs/src/components/atoms/myapp.tsx
@@ -1,27 +1,37 @@
-'use client';
 import { ThemeContext } from "@/utilities/context/mycontext";
-import { useState } from "react";
-import Navbar from "./navbar";
 import { themes } from "@/utilities/themes/mythemes";
+import { useState, useEffect } from "react";
+import Navbar from "./navbar";
 
-export default function MyApp({ Component, pageProps}: { Component: any, pageProps: any }) {
-    const [theme, setTheme] = useState(themes.light);
+export default function MyApp({ Component, pageProps }: { Component: any, pageProps: any }) {
+    const [theme, setTheme] = useState(() => {
+        const storedTheme = localStorage.getItem('theme');
+        return storedTheme ? JSON.parse(storedTheme) : themes.light;
+    });
 
     const toggleTheme = () => {
-        setTheme(theme === themes.dark ? themes.light : themes.dark);
+        const newTheme = theme === themes.dark ? themes.light : themes.dark;
+        setTheme(newTheme);
+        localStorage.setItem('theme', JSON.stringify(newTheme));
     };
 
-    return(
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme');
+        if (storedTheme && JSON.parse(storedTheme) !== theme) {
+            setTheme(JSON.parse(storedTheme));
+        }
+    }, []);
+
+    return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             <div
                 style={{
                     width: '100%',
                     minHeight: '100vh',
                     ...theme,
-            }}>
+                }}>
                 <Navbar />
                 <Component {...pageProps} />
             </div>
         </ThemeContext.Provider>
-    );
-}
+    )}
